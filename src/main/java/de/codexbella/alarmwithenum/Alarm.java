34f4115f6e2c153199f8b431aa1dd98e40b9b528
Anna Lena Baumann <rescht@posteo.de>
2022-02-15 14:17:29 +0100

    added dummy change

--- src/main/java/de/codexbella/alarmwithenum/Alarm.java
+++ src/main/java/de/codexbella/alarmwithenum/Alarm.java
@@ -2,6 +2,6 @@ package de.codexbella.alarmwithenum;
 
 public class Alarm {
     public static String customerCheck(int numberOfCustomers, AlertLevel alertLevel) {
-        return alertLevel.determineWarning(numberOfCustomers);
+        return alertLevel.determineWarning(numberOfCustomers);//test
     }
 }
